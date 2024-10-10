cbe3f57b1e8e63e669a9702bb622b43ca29c22e3
rajranjan672 <76998014+rajranjan672@users.noreply.github.com>
2024-10-10 17:15:55 +0530

    asf
    sdf

--- front-end/src/Components/Characters/Characters.jsx
+++ front-end/src/Components/Characters/Characters.jsx
@@ -1,31 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Charactors } from '../Charactors';
 import "./Characters.css";
 import Hero from '../Hero/Hero';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 
 const Characters = () => {
-    console.log(Charactors.name)
+  const [visibleDetails, setVisibleDetails] = useState(null);
+
+  const toggleDetails = (id) => {
+      setVisibleDetails(visibleDetails === id ? null : id);
+  };
+
   return (
-    <div className='container-fluid'>
-
-        <div className="row d-flex justify-content-center">
-      <Hero />
-
-        <h3 className='text-danger '>Characters</h3>
-      {Charactors.map((item) => {
-        return (
-            <div className='card h-100 col-11 col-sm-5 col-md-3 col-lg-3 mx-4 my-4 p-0' key={item.id}>
-            <img src={item.imgg} height={"300"} width={"100%"} alt='tee' />
-
-            <h4 className='mx-auto'>{item.name}</h4>
-            </div>
-
-        )
-      })}
-        </div>
-      
-    </div>
-  )
+      <div className='container-fluid'>
+          <div className="row d-flex justify-content-center">
+              <Hero />
+              <h3 className='text-danger'>Characters</h3>
+              {Charactors.map((item) => {
+                  const isVisible = visibleDetails === item.id;
+                  return (
+                      <div className='card h-100 col-11 col-sm-5 col-md-3 col-lg-3 mx-4 my-4 p-0' key={item.id}>
+                          <img src={item.imgg} height={"300"} width={"100%"} alt={item.name} />
+                          <h4 className='mx-auto'>{item.name}</h4>
+                          <button onClick={() => toggleDetails(item.id)} className="btn btn-link">
+                              <ExpandMoreIcon  />
+                              {isVisible ? ' Hide Details' : ' View More'}
+                          </button>
+                          {isVisible && <p className="details text-white">{item.details}</p>}
+                      </div>
+                  );
+              })}
+          </div>
+      </div>
+  );
 }
 
 export default Characters
--- front-end/src/Components/Charactors.js
+++ front-end/src/Components/Charactors.js
@@ -2,37 +2,38 @@ import hades from "./Assets/hades.png";
 import Persephone from "./Assets/Persephone.png";
 import Posidon from "./Assets/Poseidon.png";
 import Zeus from "./Assets/Zeus.png";
-import Eros from "./Assets/Eros.png"
-
+import Eros from "./Assets/Eros.png";
 
 export const Charactors = [
-    {
-        id:1,
-        name: "Hades",
-        imgg: hades
-    },
-     {
-        id: 2,
-        name: "Persephone",
-        imgg: Persephone
-
-     },
-     {
-        id: 3,
-        name: "Zeus",
-        imgg: Posidon
-
-     },
-     {
-        id: 4,
-        name: "Poseidon",
-        imgg: Zeus
-
-     },
-     {
-        id: 5,
-        name: "Eros",
-        imgg: Eros
-
-     }
-]
+  {
+    id: 1,
+    name: "Hades",
+    imgg: hades,
+    details:
+      "In the Lore Olympus webtoon, Hades, the God of the underworld, takes center stage. Often depicted as a handsome blue man in a business suit, Hades runs the Underworld Corporation, serves as the older brother of Zeus and Poseidon, and plays a role in the Six Traitor Dynasty. His character takes on the burden of leadership and pursuing personal interests.",
+  },
+  {
+    id: 2,
+    name: "Persephone",
+    imgg: Persephone,
+    details: "Persephone, the goddess of spring, is the story’s heroine alongside the underworld. Initially portrayed as an innocent and naive young woman, her character develops dramatically as she faces more challenges."
+  },
+  {
+    id: 3,
+    name: "Zeus",
+    imgg: Posidon,
+    details: "Zeus, the king of the gods, is depicted as a yellow-skinned man with long, straight hair. Known for his promiscuity and egotism, Zeus’ actions often brought chaos to Olympus. A member of the Six Realms of Lies, he has intimate relationships with many characters, including his brothers Hades and Poseidon, and many lovers. His character provides a lens through which to examine issues of power and corruption."
+  },
+  {
+    id: 4,
+    name: "Poseidon",
+    imgg: Zeus,
+    details: "Poseidon, the God of the sea, is Zeus’s brother from the underworld. He is usually characterized by a carefree and quick temperament, portrayed with green skin and long, wavy hair. Despite being slightly less witted, Poseidon is very loyal to his family. His relationship with his wife Amphitrite and their polygamous arrangement add an intriguing dynamic to his character."
+  },
+  {
+    id: 5,
+    name: "Eros",
+    imgg: Eros,
+    details: "Eros, the God of love and the son of Aphrodite and Ares is a strong, maroon young man. He is one of Persephone’s best friends and knows the depression she is enduring. Eros is supportive and loving, often helping to nurture other romantic relationships as he deals with his challenges, including his relationship with Psyche."
+  },
+];
